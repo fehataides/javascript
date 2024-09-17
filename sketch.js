feb34fd4a0b3180403658c1eefbd83cb96a75a84
fehataides <felipeferreiradeataides@gmail.com>
2024-09-17 16:40:26 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+    background("pink");
+  }
+  
+  function draw() {
+    stroke("white");
+    fill("purple");
+  
+    // console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 15, 20);
+    }
+  }
